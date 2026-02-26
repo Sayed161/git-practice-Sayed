85cdbabcd7a24af34538d014ba0eee0f9df1c8b8
Sayed161 <sheikhsayed23@gmail.com>
2026-02-27 01:11:02 +0600

    error handling to the calculator

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ from datetime import date
 print("Name : Sheikh Sayed")
 print(date.today())
 
+def calculator(a,b):
+    if(a>b):
+        add(a,b)
+    if(b>a):
+        subtract(a,b)
 def add(a,b):
     print(a+b)
 def subtract(a,b):
