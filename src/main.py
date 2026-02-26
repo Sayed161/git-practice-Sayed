4db6b69f2ee52423672ee28167373d894a30f9d3
Sayed161 <sheikhsayed23@gmail.com>
2026-02-27 01:01:23 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 from datetime import date
 print("Name : Sheikh Sayed")
 print(date.today())
+
+def add(a,b):
+    print(a+b)
+def subtract(a,b):
+    print(b-a)
---
+++ src/utils.py
@@ -0,0 +1,5 @@
+from main import add,subtract
+a=5
+b=10
+add(a, b)
+subtract(a, b)
